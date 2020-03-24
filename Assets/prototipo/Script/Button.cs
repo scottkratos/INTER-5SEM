5a723ddd2e7a4e23dc5e4c5f06b9deda3a0ea795
LucasCruz99 <lucasslv99@gmail.com>
2020-03-24 09:41:13 -0300

    agora vai

---
+++ Assets/prototipo/Script/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Animator anim, Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void openDoor()
+    {
+        Door.SetBool("DoorBool", true);
+        anim.speed = 0;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Vaso")
+        {
+
+            anim.SetBool("ButtonBool", true);
+
+
+        }
+    }
+}
