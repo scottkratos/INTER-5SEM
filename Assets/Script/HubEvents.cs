f373d4d39f7c094dc5ac17012578736c940261e4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 12:45:56 -0300

    Level 12

--- Assets/Script/HubEvents.cs
+++ Assets/Script/HubEvents.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HubEvents : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class HubEvents : MonoBehaviour
     private bool IsCutscene = false;
     private const float MaxShader = -5;
     private float ShaderValue;
+
 
     private void Awake()
     {
